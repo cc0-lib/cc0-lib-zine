ee9affb4598e40eafdb2693a5042473894e3233e
0xN1 <info@nero1.net>
2023-11-28 18:01:48 +0800

    add analytics, remove pnpm file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Web3Provider from "@/components/web3/web3-provider";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -15,7 +16,10 @@ export default async function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={`selection:bg-grayA selection:text-prim`}>
-        <Web3Provider>{children}</Web3Provider>
+        <Web3Provider>
+          {children}
+          <Analytics />
+        </Web3Provider>
       </body>
     </html>
   );
